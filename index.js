ba887fe41209c6d7efb772a936c2ad26b98b29da
MatiuDev <thematius.monsalve@gmail.com>
2024-01-02 17:30:10 -0500

    New: Actualizando el portafolio, cambios visuales y de funcionalidad.

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-const btnOpen = document.querySelector('#contact__btnOpen');
-const btnClose = document.querySelector('#contact__btnClose');
-const frmContact = document.querySelector('.contact__form-container');
+document.addEventListener('DOMContentLoaded', function () {
+    loadContactActions();
+    loadNavBarStyle();
 
-btnOpen.addEventListener('click', () => {
-    frmContact.style.left = 0;
+    function loadContactActions() {
+        const btnOpen = document.querySelector('#contact__btnOpen');
+        const btnClose = document.querySelector('#contact__btnClose');
+        const frmContact = document.querySelector('.contact__form-container');
+        const { body, documentElement }= document;
+
+        btnOpen.addEventListener('click', () => {
+            const scrollHeight = Math.max(body.scrollHeight, documentElement.scrollHeight);
+            window.scrollTo({ top: scrollHeight });
+            frmContact.style.left = 0;
+            body.style.overflowY = 'hidden';
+        });
+        
+        btnClose.addEventListener('click', () => {
+            frmContact.style.left = '-2000px';
+            body.style.overflowY = 'scroll';
+        });
+    }
+
+    function loadNavBarStyle() {
+        const navDesktop = document.querySelector('.nav__desktop');
+        const homeSection = document.querySelector('.home__section');
+        const nextSection = homeSection.nextElementSibling;
+        const nextSectionPosY = nextSection.offsetTop;
+
+        window.addEventListener('scroll', function () {
+            console.log('here');
+            if ((window.scrollY + 100) > nextSectionPosY) {
+                navDesktop.style.backgroundColor = '#1f45548f';
+                
+            } else {
+                navDesktop.style.backgroundColor = 'transparent';
+            }
+        });
+    }    
 });
 
-btnClose.addEventListener('click', () => {
-    frmContact.style.left = '-2000px';
-});
