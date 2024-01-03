2b89f70b17bad536708b1616e61ca88f143b4941
MatiuDev <thematius.monsalve@gmail.com>
2024-01-03 14:00:55 -0500

    New: Agregando funcionalidad dinámica del formulario y año del pie de página

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     loadContactActions();
     loadNavBarStyle();
+    loadYear();
 
     function loadContactActions() {
         const btnOpen = document.querySelector('#contact__btnOpen');
         const btnClose = document.querySelector('#contact__btnClose');
-        const frmContact = document.querySelector('.contact__form-container');
-        const { body, documentElement }= document;
+        const btnSubmit = document.querySelector('#btnSubmit__contact');
 
-        btnOpen.addEventListener('click', () => {
-            const scrollHeight = Math.max(body.scrollHeight, documentElement.scrollHeight);
-            window.scrollTo({ top: scrollHeight });
-            frmContact.style.left = 0;
-            body.style.overflowY = 'hidden';
-        });
-        
-        btnClose.addEventListener('click', () => {
-            frmContact.style.left = '-2000px';
-            body.style.overflowY = 'scroll';
-        });
+        btnOpen.addEventListener('click', openContactForm);
+        btnClose.addEventListener('click', closeContactForm);
+        btnSubmit.addEventListener('click', SendMessageForm);
     }
 
     function loadNavBarStyle() {
@@ -28,7 +20,6 @@ document.addEventListener('DOMContentLoaded', function () {
         const nextSectionPosY = nextSection.offsetTop;
 
         window.addEventListener('scroll', function () {
-            console.log('here');
             if ((window.scrollY + 100) > nextSectionPosY) {
                 navDesktop.style.backgroundColor = '#1f45548f';
                 
@@ -36,6 +27,79 @@ document.addEventListener('DOMContentLoaded', function () {
                 navDesktop.style.backgroundColor = 'transparent';
             }
         });
-    }    
+    }
+
+    function loadYear() {
+        const spanYear = document.getElementById('contact__year');
+        const date = new Date();
+        spanYear.innerHTML = date.getFullYear();
+    }
+
+    function openContactForm() {
+        const frmContact = document.querySelector('.contact__form-container');
+        const { body, documentElement }= document;
+        const scrollHeight = Math.max(body.scrollHeight, documentElement.scrollHeight);
+            window.scrollTo({ top: scrollHeight });
+            frmContact.style.left = 0;
+            body.style.overflowY = 'hidden';
+    }
+
+    function closeContactForm() {
+        const frmContact = document.querySelector('.contact__form-container');
+        frmContact.style.left = '-2000px';
+        document.body.style.overflowY = 'scroll';
+    }
+
+    function cleanContactForm() {
+        document.getElementById('name').value = '';
+        document.getElementById('email').value = '';
+        document.getElementById('message').value = '';
+    }
+
+    async function SendMessageForm() {
+        const inputName = document.getElementById('name').value;
+        const inputEmail = document.getElementById('email').value;
+        const inputMessage = document.getElementById('message').value;
+            
+        await sendEmail(inputName, inputEmail, inputMessage);
+        cleanContactForm();
+        closeContactForm();
+        window.location.hash = '#';
+        window.location.reload();
+    }
+
+    async function sendEmail(name, email, message) {
+        const url = 'https://mailapi-production.up.railway.app/sendmail';
+        try {
+            const payload = {
+                name,
+                email,
+                message
+            };
+
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(payload)
+            });
+            
+            if (!response.ok) {
+                console.log('Hubo un error al enviar la petición.');
+                return;
+            }
+
+            const data = await response.json();
+            const { status, error } = data;
+
+            if (status === 'fail') {
+                console.log(`Error: ${error}`)
+                return;
+            }
+
+            console.log('El mensaje ha sido enviado correctamente, gracias por su interés.');
+        } catch (err) {
+            console.error('Error en la petición:', err)
+        }
+    }
 });
 
